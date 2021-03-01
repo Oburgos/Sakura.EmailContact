ba0b7a840a8453fe410c6fb73907119f29a59a11
Omar Burgos Bueso <omarburgosb@gmail.com>
2021-02-28 23:06:26 -0600

    OnDestroy Event to remove the mjml editor and asset manager to upload files
    1. OnDestroy added to remove the grapesJS editor globally.
    2. Asset Manager configuration to upload files to s3 through the API.
    3. Remove the key gjs-components from local storage when the email-template component is created.

--- FrontEnd/SakuraEmailAngular/src/app/email-templates/email-template-builder/email-template-builder.component.ts
+++ FrontEnd/SakuraEmailAngular/src/app/email-templates/email-template-builder/email-template-builder.component.ts
@@ -2,6 +2,7 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
+  OnDestroy,
   OnInit,
   ViewChild,
 } from '@angular/core';
@@ -10,36 +11,61 @@ import 'grapesjs/dist/css/grapes.min.css';
 import grapesJS from 'grapesjs';
 // @ts-ignore
 import grapesJSMJML from 'grapesjs-mjml';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-email-template-builder',
   templateUrl: './email-template-builder.component.html',
   styleUrls: ['./email-template-builder.component.scss'],
 })
-export class EmailTemplateBuilderComponent implements OnInit, AfterViewInit {
+export class EmailTemplateBuilderComponent
+  implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('gjs', { static: true }) gjs!: ElementRef;
-
+  grapes: any = {};
   constructor() {}
 
+  ngOnDestroy(): void {
+    localStorage.removeItem('gjs-components');
+    this.grapes.destroy();
+  }
+
   ngOnInit(): void {}
+
+  checkInfo() {
+    let html = this.grapes.runCommand('mjml-get-code');
+    let mjml = this.grapes.getHtml();
+    console.log(html, mjml);
+  }
 
   ngAfterViewInit(): void {
     this.crearControl();
   }
 
   crearControl(): void {
-    this.gjs.nativeElement.innerHTML =
-      '<mjml><mj-body><mj-section><mj-column></mj-column></mj-section></mj-body></mjml>';
+    localStorage.removeItem('gjs-components');
+    this.gjs.nativeElement.innerHTML = `
+    <mjml>
+       <mj-body>
+           <mj-section>
+              <mj-column>
+                <mj-button id="iu1o">Button</mj-button>
+              </mj-column>
+           </mj-section>
+        </mj-body>
+     </mjml>`;
 
-    grapesJS.init({
+    this.grapes = grapesJS.init({
       fromElement: 1,
       container: '#gjs',
       avoidInlineStyle: false,
       plugins: [grapesJSMJML],
+      assetManager: {
+        upload: environment.Api + '/api/common/images/upload',
+        uploadName: 'files',
+        credentials: 'omit',
+      },
       pluginsOpts: {
-        [grapesJSMJML]: {
-          /* ...options */
-        },
+        [grapesJSMJML]: {},
       },
     });
   }
